260d1bde67a50e1e5de469bede576e3372c06261
my_username <my_email_address>
2023-01-04 09:38:17 +0300

    Copy and pasted code

--- Test game/HeloWorld.py
+++ Test game/HeloWorld.py
@@ -1,24 +1,55 @@
-#code from https://www.javatpoint.com/how-to-develop-a-game-in-python
-import pygame
-from math import pi
-
+import pygame    
+from math import pi    
 pygame.init()    
-screen = pygame.display.set_mode((400,500))
-pygame.display.set_caption("Pixel Game")    
-done = False   
-clock = pygame.time.Clock()  
-    
+# size variable is using for set screen size    
+size = [400, 300]    
+screen = pygame.display.set_mode(size)    
+pygame.display.set_caption("Example program to draw geometry")    
+# done variable is using as flag     
+done = False    
+clock = pygame.time.Clock()    
 while not done:    
-    for event in pygame.event.get():    
-        if event.type == pygame.QUIT:    
-            done = True    
-    pygame.display.flip()
+    # clock.tick() limits the while loop to a max of 10 times per second.    
+    clock.tick(10)    
+        
+    for event in pygame.event.get():  # User did something    
+        if event.type == pygame.QUIT:  # If user clicked on close symbol     
+            done = True  # done variable that we are complete, so we exit this loop    
+    
+    # All drawing code occurs after the for loop and but    
+    # inside the main while done==False loop.    
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        done = True
+    # Clear the default screen background and set the white screen background
+    screen.fill((255, 255, 255))    
 
-screen.fill((0, 0, 0)) 
+    # Draw on the screen a green line which is 5 pixels wide.
+    pygame.draw.line(screen, (0, 255, 0), [0, 0], [50, 30], 5)
+    # Draw on the screen a green line which is 5 pixels wide.    
+    pygame.draw.lines(screen, (0, 0, 0), False, [[0, 80], [50, 90], [200, 80], [220, 30]], 5)    
 
-# Draw on the screen a green line which is 5 pixels wide.    
-pygame.draw.line(screen, (0, 255, 0), [0, 0], [50, 30], 5)
+    # Draw a rectangle outline    
+    pygame.draw.rect(screen, (0, 0, 0), [75, 10, 50, 20], 2)    
+
+    # Draw a solid rectangle    
+    pygame.draw.rect(screen, (0, 0, 0), [150, 10, 50, 20])    
+
+    # This draw an ellipse outline, using a rectangle as the outside boundaries    
+    pygame.draw.ellipse(screen, (255, 0, 0), [225, 10, 50, 20], 2)    
+
+    # This draw a solid ellipse, using a rectangle as the outside boundaries    
+    pygame.draw.ellipse(screen, (255, 0, 0), [300, 10, 50, 20])    
+
+    # Draw a triangle using the polygon function    
+    pygame.draw.polygon(screen, (0, 0, 0), [[100, 100], [0, 200], [200, 200]], 5)    
+
+    # This draw a circle    
+    pygame.draw.circle(screen, (0, 0, 255), [60, 250], 40)    
+
+    # This draw an arc    
+    pygame.draw.arc(screen, (0, 0, 0), [210, 75, 150, 125], 0, pi / 2, 2)    
+
+    # This function must write after all the other drawing commands.    
+    pygame.display.flip()    
+    
+# Quite the execution when clicking on close    
+pygame.quit()
