9f6ce7da184ef150ffe1eb7bbab2f875e4c2409e
my_username <my_email_address>
2023-01-04 08:50:34 +0300

    Implomenting drowing code atempt 1

--- Test game/HeloWorld.py
+++ Test game/HeloWorld.py
@@ -1,11 +1,24 @@
+#code from https://www.javatpoint.com/how-to-develop-a-game-in-python
 import pygame
+from math import pi
 
 pygame.init()    
-screen = pygame.display.set_mode((400,500))    
-done = False    
+screen = pygame.display.set_mode((400,500))
+pygame.display.set_caption("Pixel Game")    
+done = False   
+clock = pygame.time.Clock()  
     
 while not done:    
     for event in pygame.event.get():    
         if event.type == pygame.QUIT:    
             done = True    
-    pygame.display.flip()    
+    pygame.display.flip()
+
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        done = True
+
+screen.fill((0, 0, 0)) 
+
+# Draw on the screen a green line which is 5 pixels wide.    
+pygame.draw.line(screen, (0, 255, 0), [0, 0], [50, 30], 5)
